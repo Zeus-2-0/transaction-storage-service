05b38192231f7f0c105ce532624c59c86c7be875
vbalaji215-dev <vbalaji215@outlook.com>
2022-03-16 16:12:29 -0400

    Initial Add

---
+++ src/main/java/com/brihaspathee/zeus/TransactionStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brihaspathee.zeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionStorageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionStorageServiceApplication.class, args);
+    }
+
+}
